5e5fc94b2e9d31d298aaca695ee07f438f460dd2
rawadabu <rawadabusaleh0@gmail.com>
2023-10-27 16:20:56 +0300

    Updating css and pages 💥

--- world-travel/src/pages/Homepage.jsx
+++ world-travel/src/pages/Homepage.jsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
-import PageNav from "../components/pages/PageNav";
-import AppNav from "../components/pages/AppNav";
+import styles from "./Homepage.module.css";
 
-function Homepage() {
+export default function Homepage() {
   return (
-    <div>
-      <PageNav />
-      <AppNav />
-      <h1 className="test">World Travel</h1>
-
-      <Link to="layout">Go to the app layout</Link>
-    </div>
+    <main className={styles.homepage}>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to="/layout" className="cta">
+          Start tracking now
+        </Link>
+      </section>
+    </main>
   );
 }
-
-export default Homepage;
